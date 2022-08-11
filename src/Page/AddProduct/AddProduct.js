faa368b66273ae2f5e7ee3fffe3379e1262ab0e2
Mahed21 <mahed255261@gmail.com>
2022-08-11 21:53:26 +0600

    second commit

--- src/Page/AddProduct/AddProduct.js
+++ src/Page/AddProduct/AddProduct.js
@@ -1,15 +1,43 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./AddProduct.css";
+import { useNavigate } from "react-router";
 
 const AddProduct = () => {
-  const [selects, setSelect] = useState(null);
+  let navigate = useNavigate();
+  const [selects, setSelect] = useState("00");
+  const [sku, setSku] = useState("00");
+  const [name, setName] = useState("00");
+  const [price, setPrice] = useState("00");
+  const [size, setSize] = useState("00");
+  const [weight, setweight] = useState("00");
+  const [height, setHeight] = useState("00");
+  const [width, setWidth] = useState("00");
+  const [length, setLength] = useState("00");
+
+  const addProduct = () => {
+    axios
+      .post("http://localhost:3002/product", {
+        sku: sku,
+        name: name,
+        price: price,
+        size: size,
+        weight: weight,
+        height: height,
+        width: width,
+        length: length,
+      })
+      .then(() => {
+        console.log("success");
+      });
+  };
   return (
     <div className="container mt-5">
-      <nav class="navbar navbar-light">
-        <div class="container-fluid">
-          <a class="navbar-brand">Product Add</a>
-          <form class="d-flex">
+      <nav className="navbar navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Product Add</a>
+          <form className="d-flex">
             <Link to="" className="me-2">
               <button className="btn">Save</button>
             </Link>
@@ -20,52 +48,55 @@ const AddProduct = () => {
         </div>
       </nav>
       <hr />
-      <div class="mb-3 row">
+      <div className="mb-3 row">
         <div className="col-2 col-lg-1">
-          <label for="inputPassword" class="col-form-label">
+          <label for="inputPassword" className="col-form-label">
             SKU
           </label>
         </div>
-        <div class="col-10 col-lg-11">
+        <div className="col-10 col-lg-11">
           <input
-            type="password"
-            class="form-control w-25 input"
+            type="text"
+            className="form-control w-25 input"
             id="inputPassword"
+            onChange={(e) => setSku(e.target.value)}
           />
         </div>
       </div>
 
-      <div class="mb-3 row">
-        <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-2 col-lg-1 col-form-label">
           Name
         </label>
-        <div class="col-10 col-lg-11">
+        <div className="col-10 col-lg-11">
           <input
-            type="password"
-            class="form-control w-25 input"
+            type="text"
+            className="form-control w-25 input"
             id="inputPassword"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
       </div>
 
-      <div class="mb-3 row">
-        <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-2 col-lg-1 col-form-label">
           Price
         </label>
-        <div class="col-10 col-lg-11">
+        <div className="col-10 col-lg-11">
           <input
-            type="password"
-            class="form-control w-25 input"
+            type="text"
+            className="form-control w-25 input"
             id="inputPassword"
+            onChange={(e) => setPrice(e.target.value)}
           />
         </div>
       </div>
 
-      <div class="mb-3 row">
-        <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-2 col-lg-1 col-form-label">
           Type Switcher
         </label>
-        <div class="col-10 col-lg-11">
+        <div className="col-10 col-lg-11">
           <select
             className="input w-25"
             value={selects}
@@ -83,73 +114,90 @@ const AddProduct = () => {
       {selects === "DVD" ? (
         <div>
           {" "}
-          <div class="mb-3 row">
-            <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+          <div className="mb-3 row">
+            <label
+              for="inputPassword"
+              className="col-2 col-lg-1 col-form-label"
+            >
               Size (MB)
             </label>
-            <div class="col-10 col-lg-11">
+            <div className="col-10 col-lg-11">
               <input
-                type="password"
-                class="form-control w-25 input"
+                type="text"
+                className="form-control w-25 input"
                 id="inputPassword"
                 placeholder="#size"
+                onChange={(e) => setSize(e.target.value)}
               />
             </div>
           </div>
         </div>
       ) : selects === "Book" ? (
         <div>
-          <div class="mb-3 row">
-            <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+          <div className="mb-3 row">
+            <label
+              for="inputPassword"
+              className="col-2 col-lg-1 col-form-label"
+            >
               Height (CM)
             </label>
-            <div class="col-10 col-lg-11">
+            <div className="col-10 col-lg-11">
               <input
-                type="password"
-                class="form-control w-25 input"
+                type="text"
+                className="form-control w-25 input"
                 id="inputPassword"
                 placeholder="#height"
+                onChange={(e) => setHeight(e.target.value)}
               />
             </div>
           </div>
-          <div class="mb-3 row">
-            <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+          <div className="mb-3 row">
+            <label
+              for="inputPassword"
+              className="col-2 col-lg-1 col-form-label"
+            >
               width (CM)
             </label>
-            <div class="col-10 col-lg-11">
+            <div className="col-10 col-lg-11">
               <input
-                type="password"
-                class="form-control w-25 input"
+                type="text"
+                className="form-control w-25 input"
                 id="inputPassword"
                 placeholder="#width"
+                onChange={(e) => setWidth(e.target.value)}
               />
             </div>
           </div>
-          <div class="mb-3 row">
-            <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+          <div className="mb-3 row">
+            <label
+              for="inputPassword"
+              className="col-2 col-lg-1 col-form-label"
+            >
               length (CM)
             </label>
-            <div class="col-10 col-lg-11">
+            <div className="col-10 col-lg-11">
               <input
-                type="password"
-                class="form-control w-25 input"
+                type="text"
+                className="form-control w-25 input"
                 id="inputPassword"
                 placeholder="#length"
+                onChange={(e) => setLength(e.target.value)}
               />
             </div>
           </div>
         </div>
       ) : selects === "Furniture" ? (
-        <div class="mb-3 row">
-          <label for="inputPassword" class="col-2 col-lg-1 col-form-label">
+        <div className="mb-3 row">
+          <label for="inputPassword" className="col-2 col-lg-1 col-form-label">
             Weight (Kg)
           </label>
-          <div class="col-10 col-lg-11">
+          <div className="col-10 col-lg-11">
             <input
-              type="password"
-              class="form-control w-25 input"
+              type="text"
+              className="form-control w-25 input"
               id="inputPassword"
               placeholder="#weight"
+              onChange={(e) => setweight(e.target.value)}
             />
           </div>
         </div>
@@ -157,7 +205,9 @@ const AddProduct = () => {
         ""
       )}
       <br />
-      <button className="btn">Submit</button>
+      <button className="btn" onClick={addProduct}>
+        Submit
+      </button>
     </div>
   );
 };
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="container mt-5">
-      <nav class="navbar navbar-light">
-        <div class="container-fluid">
-          <a class="navbar-brand">Product List</a>
-          <form class="d-flex">
+      <nav className="navbar navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Product List</a>
+          <form className="d-flex">
             <Link to="/addproduct" className="me-2">
               <button className="btn">ADD</button>
             </Link>
